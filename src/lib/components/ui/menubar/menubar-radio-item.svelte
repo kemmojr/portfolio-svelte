e40518460c464e86afc94dcd05f86f653128df0d
Tim Kemmis <timothydkemmis@gmail.com>
2025-03-01 22:22:32 +1100

    🥾 stamping out the old icon dep

--- src/lib/components/ui/menubar/menubar-radio-item.svelte
+++ src/lib/components/ui/menubar/menubar-radio-item.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import { Menubar as MenubarPrimitive } from "bits-ui";
-	import DotFilled from "svelte-radix/DotFilled.svelte";
-	import { cn } from "$lib/utils.js";
+	import { Menubar as MenubarPrimitive } from 'bits-ui';
+	import CircleDot from 'lucide-svelte/icons/circle-dot';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = MenubarPrimitive.RadioItemProps;
 	type $$Events = MenubarPrimitive.RadioItemEvents;
-	let className: $$Props["class"] = undefined;
-	export let value: $$Props["value"];
+	let className: $$Props['class'] = undefined;
+	export let value: $$Props['value'];
 	export { className as class };
 </script>
 
 <MenubarPrimitive.RadioItem
 	{value}
 	class={cn(
-		"data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+		'data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 		className
 	)}
 	{...$$restProps}
@@ -27,7 +27,7 @@
 >
 	<span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 		<MenubarPrimitive.RadioIndicator>
-			<DotFilled class="h-4 w-4 fill-current" />
+			<CircleDot class="h-4 w-4 fill-current" />
 		</MenubarPrimitive.RadioIndicator>
 	</span>
 	<slot />
