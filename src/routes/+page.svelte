2d98f82a09eead932327004f066e9bcad2eae2a1
Tim Kemmis <timothydkemmis@gmail.com>
2025-03-01 22:15:41 +1100

    🔎 implemented shadcn ui svelte with lucide-svelte icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Menubar from '$lib/components/ui/menubar';
+</script>
+
+<Menubar.Root>
+	<Menubar.Menu>
+		<Menubar.Trigger>File</Menubar.Trigger>
+		<Menubar.Content>
+			<Menubar.Item>
+				New Tab
+				<Menubar.Shortcut>⌘T</Menubar.Shortcut>
+			</Menubar.Item>
+			<Menubar.Item>New Window</Menubar.Item>
+			<Menubar.Separator />
+			<Menubar.Item>Share</Menubar.Item>
+			<Menubar.Separator />
+			<Menubar.Item>Print</Menubar.Item>
+		</Menubar.Content>
+	</Menubar.Menu>
+</Menubar.Root>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me</Button>
